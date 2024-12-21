807f08f03951805add7d0307ba42dce5fc186cf6
kafae <104088216+qkafae@users.noreply.github.com>
2024-12-21 23:40:44 +0800

    Delete CSMPSacks directory

--- CSMPSacks/src/main/kotlin/ovh/csmp/cSMPSacks/sacks.kt
+++
@@ -1,6 +0,0 @@
-package ovh.csmp.cSMPSacks
-
-object sacks {
-    val travellers_sack = sack("Traveller's Sack", "travellers_sack", "COMMON", 2000, 100, 1)
-    val illusion_sack = sack("Illusion Sack", "illusion_sack", "UNCOMMON", 2001, 200, 2)
-}
